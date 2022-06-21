d2ac55dd2d6eda2ad6b5c12eda7eb086648c00c6
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 00:17:11 +0300

    create logout and signup urls

--- loanapp/urls.py
+++ loanapp/urls.py
@@ -4,7 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('',views.index,name = 'index'),
+    path('signup/', views.signup, name= 'signup'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('services/', views.services, name='services')
 
 ]
