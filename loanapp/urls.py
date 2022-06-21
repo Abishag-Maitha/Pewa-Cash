4f8890925763048663207a2179320421b6de2aa3
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 01:08:33 +0300

    Add account and update account urls

--- loanapp/urls.py
+++ loanapp/urls.py
@@ -7,7 +7,10 @@ urlpatterns=[
     path('signup/', views.signup, name= 'signup'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
-     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('account/<id>', views.account, name='account'),
+    path('updateaccount/<id>', views.updateaccount, name='updateaccount'),
+
     path('services/', views.services, name='services')
 
 ]
