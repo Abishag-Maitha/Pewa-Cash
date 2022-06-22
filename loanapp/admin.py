25ca41f86996b5cf18ef1ccc84da4ca1a7921e11
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 18:34:49 +0300

    Register models

--- loanapp/admin.py
+++ loanapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Account, Loan,Payment
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Loan)
+admin.site.register(Payment)
