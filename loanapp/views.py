c18fd47a89db8b6d356ff659dc1b02fb881e99ec
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 18:35:33 +0300

    create user update form

--- loanapp/views.py
+++ loanapp/views.py
@@ -17,43 +17,33 @@ def services(request):
 def signup(request):
     if request.method=="POST":
         form=RegistrationForm(request.POST)
-        procForm=AccountForm(request.POST, request.FILES)
-        if form.is_valid() and procForm.is_valid():
+        if form.is_valid():
             username=form.cleaned_data.get('username')
             user=form.save()
-            account=procForm.save(commit=False)
-            account.user=user
-            account.save()
-
+           
         return redirect('login')
     else:
         form= RegistrationForm()
-        prof=AccountForm()
     params={
-        'form':form,
-        'profForm': prof
+        'form':form
     }
     return render(request, 'auth/signup.html', params)
 
 def account(request,id):
     acc = Account.objects.get(id = id)
     return render(request,'user_account.html',{"account":acc})
-
-def updateaccount(request):
-    user= request.user
-    if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST, instance=request.user)
-        acc_form = UserAccountUpdateForm(request.POST, request.FILES, instance=request.user.account)
-        if user_form.is_valid() and  acc_form.is_valid():
-            user_form.save()
-            acc_form.save()
-            return redirect('account', user.id)
-    else:
-        user_form = UserUpdateForm(instance=request.user)
-        acc_form = UserAccountUpdateForm(instance=request.user.profile)
-    params = {
-        'user_form': user_form,
-        'acc_form': acc_form
-    }
-    return render(request, 'update_account.html', params)
     
+@login_required(login_url='login')  
+def updateaccount(request,id):
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAccountUpdateForm(request.POST or None,request.FILES or None, instance=user.account)
+        if form.is_valid():
+            form.save()
+            return redirect('account', request.user.id)
+    else:
+        form = UserAccountUpdateForm()
+        return render(request, 'update_account.html',{"form":form })
+
+
+
