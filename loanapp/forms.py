ccafe53966e4b4f05506c5b424dc195d7c6404a1
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 23:45:51 +0300

    loan request and loan replyment form

--- loanapp/forms.py
+++ loanapp/forms.py
@@ -1,46 +1,14 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from .models import Account, Loan, Payment
+from .models import loanRequest, loanTransaction
 
 
-class AccountForm(forms.ModelForm):
+class LoanRequestForm(forms.ModelForm):
     class Meta:
-        model = Account
-        fields = ['Full_Name', 'MobileNo', 'ID_card']
+        model = loanRequest
+        fields = ('category', 'reason', 'amount', 'year')
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+
+class LoanTransactionForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username','email'] 
-
-class UserAccountUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-    class Meta:
-        model = Account
-        fields = ['email', 'photo', 'MobileNo']
-
-class LoanForm(forms.ModelForm):
-    class Meta:
-        model = Loan
-        fields = ['Loan_Amount','Loan_Term']
-
-class PaymentForm(forms.ModelForm):
-    class Meta:
-        model = Payment
-        fields = ['Amount']
-        
-class RegistrationForm(UserCreationForm):
-    email=forms.EmailField()
-    class Meta:
-        model = User
-        fields = ['username', 'email','password1', 'password2']
-
-    def save(self, commit=True):
-        user=super().save(commit=False)
-        user.email=self.cleaned_data['email']
-        if commit:
-
-            user.save()
-        return user
+        model = loanTransaction
+        fields = ('payment',)
