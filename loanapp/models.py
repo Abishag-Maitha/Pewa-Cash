b98304489c4d09c6df3bf76cd94e105a4b9d8bd8
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-22 18:35:06 +0300

    Remove unused models

--- loanapp/models.py
+++ loanapp/models.py
@@ -1,17 +1,15 @@
 import uuid
 from django.db import models
-from decimal import ROUND_HALF_UP, Decimal
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from django.forms import DecimalField
-from django.utils import timezone
 from django.utils.translation import gettext as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
-class Account(models.Model):
-    name=models.TextField(max_length=100)
+class Account(models.Model):  
+    Full_Name=models.CharField(max_length=100)
     photo = CloudinaryField('image')
     email = models.EmailField(max_length=30, blank=True)
     user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='account')
@@ -20,13 +18,23 @@ class Account(models.Model):
     datecreated= models.DateField(auto_now_add=True )
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} Account'
+
+    @receiver(post_save, sender=User)
+    def create_user_account(sender, instance, created, **kwargs):
+        if created:
+            Account.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_account(sender, instance, **kwargs):
+        instance.account.save()
  
-    def save_profile(self):
+    def save_account(self):
         self.user
 
-    def delete_profile(self):
+    def delete_account(self):
         self.delete()
+
 
 LOAN_LIMITS = [
 (1,'5000'),
@@ -37,10 +45,17 @@ LOAN_LIMITS = [
 (6,'30000'),
 
 ]
+LOAN_TERM = [
+(1,'1'),
+(2,'3'),
+(3,'4'),
+(4,'6'),
+]
+
 class Loan(models.Model):
     Loan_Amount=models.PositiveIntegerField(choices = LOAN_LIMITS,default= 0)
     user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='loan')
-    Loan_Term=models.IntegerField()
+    Loan_Term=models.IntegerField(choices=LOAN_TERM, default=0)
     date_borrowed=models.DateField(auto_now_add=True)
     rate = models.DecimalField(max_digits=20, decimal_places=2)
     
